2794776658794803973ccf2321b7191ea01959bd
minjudev <aamj0226@gmail.com>
2021-09-16 22:12:04 +0900

    Add Movie component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,46 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
 // React class 컴포넌트
 // react는 자동적으로 class component의 render method를 실행
 class App extends React.Component {
-  // render 실행 전 먼저 실행됨
-  constructor(props) {
-    super(props);
-    console.log('hello');
-  }
-  // state -> object, component의 data를 넣을 공간이 있고 그 data는 변함
-  // 바꿀 데이터를 state 안에 넣기
-  state = {
-    count: 0
-  }
-  add = () => {
-    // setState 호출 시 react는 state를 refresh하고 render를 재호출함
-    // setState는 새로운 state(obj)를 받아야함
-    // this.setState({count: this.state.count + 1});
-    // current를 이용해 현재 state 얻기
-    this.setState(current => ({count: current.count + 1}));
-  };
-  minus = () => {
-    this.setState(current => ({count: current.count - 1}));
-  };
-  componentDidMount() {
-    console.log('component rendered');
-  }
-  componentDidUpdate() {
-    console.log('component updated');
-  }
   // react component는 render 메소드를 가짐
+  state = {
+    isLoading: true
+  }
+  // 비동기 방식
+  getMovies = async () => {
+    const {data: {data: {movies}}} = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by');
+    // this.setState({movies: movies});
+    this.setState({ movies, isLoading: false });
+  }
+  componentDidMount() {
+    this.getMovies();
+  }
   render() {
-    console.log('render');
+    const { isLoading, movies } = this.state;
     return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map(movie => (
+              <Movie 
+                key={movie.id} 
+                id={movie.id} 
+                year={movie.year} 
+                title={movie.title} 
+                summary={movie.summary} 
+                poster={movie.medium_cover_image}>
+              </Movie>
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
