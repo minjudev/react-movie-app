f5b4944c272b7ced5b3c5ad485c8a51ae20029b1
minjudev <aamj0226@gmail.com>
2021-09-17 15:20:49 +0900

    Complete Movie web service

--- src/App.js
+++ src/App.js
@@ -1,54 +1,21 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Navigation from "./components/Navigation";
+import Detail from "./routes/Detail";
 import "./App.css";
 
-// React className 컴포넌트
-// react는 자동적으로 className component의 render method를 실행
-class App extends React.Component {
-  // react component는 render 메소드를 가짐
-  state = {
-    isLoading: true,
-  };
-  // 비동기 방식
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by");
-    // this.setState({movies: movies});
-    this.setState({ movies, isLoading: false });
-  };
-  componentDidMount() {
-    this.getMovies();
-  }
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              ></Movie>
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      {/* Link는 router 안에 있어야 함 */}
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
+    </HashRouter>
+  );
 }
 
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,55 @@
+import React from "react";
+import axios from "axios";
+import Movie from "../components/Movie";
+import "./Home.css";
+
+// React className 컴포넌트
+// react는 자동적으로 className component의 render method를 실행
+// react hook -> state를 갖기 위해 class component 가질 필요X
+class Home extends React.Component {
+  // react component는 render 메소드를 가짐
+  state = {
+    isLoading: true,
+  };
+  // 비동기 방식
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by");
+    // this.setState({movies: movies});
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              ></Movie>
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
+}
+
+export default Home;
